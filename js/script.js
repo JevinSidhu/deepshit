c7ee88a259596e8579c1ef4f47e8396971e0f358
Jevin Sidhu <jevin.sidhu@gmail.com>
2016-01-30 05:34:39 +0000

    Add posting

--- js/script.js
+++ js/script.js
@@ -2,6 +2,46 @@ var ref = new Firebase("https://deepshit.firebaseio.com/");
 
 var accessAuthData;
 
+var olderPost;
+
+window.onload = function() {
+  
+      ref.once("value", function(snapshot) {
+      olderPost = snapshot.val();
+    
+      for(var key in olderPost){
+        
+        console.log(key);
+          
+        var outerDiv = document.createElement('div');
+        outerDiv.id = "outer";
+        document.getElementsByClassName('content-display')[0].appendChild(outerDiv);
+          
+        var createPost = document.createElement('h2');
+        var createButton = document.createElement('button');
+        var createAuthor = document.createElement('p');
+        
+        createPost.id = "post";
+        createButton.className = "votes";
+        createButton.textContent = "Love";
+        createAuthor.className = "detail-author";
+        
+        createPost.innerHTML = olderPost[key].content;
+        createAuthor.innerHTML = olderPost[key].author;
+        
+        outerDiv.appendChild(createAuthor);
+        outerDiv.appendChild(createPost);
+        outerDiv.appendChild(createButton);
+  
+  }
+        
+        }, function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        });
+        
+}
+
+
 $("#login").click(function(){
   ref.authWithOAuthPopup("twitter", function(error, authData) {
     if (error) {
@@ -9,24 +49,21 @@ $("#login").click(function(){
     } else {
       console.log(authData.twitter.displayName);
       accessAuthData = authData;
+      $("#name").html("Welcome, " + authData.twitter.displayName);
     } }, {
       remember: "default"
   });
+
 });
 
 $("#poster").click(function(){
   
-  var usersRef = ref.child("users");
-  usersRef.set({
-    post: {
+  var newPostRef = ref.push({
       author: accessAuthData.twitter.displayName,
       content: $("#content-input").val()
-    }
-
   });
   
-  usersRef.child("post").on("value", function(snapshot) {
-    
+    ref.once("child_added", function(snapshot) {
     var newPost = snapshot.val();
     
     var outerDiv = document.createElement('div');
@@ -47,12 +84,10 @@ $("#poster").click(function(){
     outerDiv.appendChild(createAuthor);
     outerDiv.appendChild(createPost);
     outerDiv.appendChild(createButton);
+    
+  });
   
 });
-  
-});
-
-
 
 
 
