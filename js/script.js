0163b1171775192f1b151a0c16c9fdf4032c1c4d
Jevin Sidhu <jevin.sidhu@gmail.com>
2016-01-30 01:20:17 +0000

    Query data into Firebase

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 var ref = new Firebase("https://deepshit.firebaseio.com/");
 
+var accessAuthData;
+
 $("#login").click(function(){
   ref.authWithOAuthPopup("twitter", function(error, authData) {
     if (error) {
       console.log("Login Failed!", error);
     } else {
-      console.log("Authenticated successfully with payload:", authData);
       console.log(authData.twitter.displayName);
+      accessAuthData = authData;
     }
   });
 });
+
+
+$("#poster").click(function(){
+  
+  var usersRef = ref.child("users");
+  usersRef.set({
+    post: {
+      author: accessAuthData.twitter.displayName,
+      content: $("#content-input").val()
+    }
+
+  });
+
+});
+
 
 
 
