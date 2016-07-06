b874e2db65005f2e8284b485df7b997bc05e0bc5
Jevin Sidhu <jevin.sidhu@gmail.com>
2016-07-06 17:04:42 +0000

    Firebase nearly complete

---
+++ js/admin.js
@@ -0,0 +1,172 @@
+/* global Firebase*/
+/* global votingFunction*/
+/* global sortSetting*/
+var approvedRef = new Firebase("https://deepshit.firebaseio.com/approved");
+var pendingRef = new Firebase("https://deepshit.firebaseio.com/pending");
+
+
+
+var accessAuthData;
+var olderPost;
+var newPost;
+var newItems = false;
+var sortSetting
+
+if (sessionStorage.selection == "sortByDate") {
+  $("#dates").attr("selected", "selected")
+  sortSetting = "dates"
+}
+
+function votingFunction() {
+ $(".votes").unbind().on("click",function() {
+ if (localStorage.authDataName == undefined) {
+     alert("fuck outta here, sign in.")
+     return }
+     var idOfPost = this.dataset.id
+     var buttonType = this.textContent
+     var removeThis = new Firebase(pendingRef + "/" + idOfPost)
+ if (buttonType == "Approve") {
+   pendingRef.on("value", function(snapshot) {
+     var object = snapshot.val()
+     var approved = object[idOfPost]
+     approvedRef.push(approved, function(error) {
+       if (error) {
+         alert("Post failed to transfer!")
+       }
+       else {
+         removeThis.remove()
+         alert("approved!")
+       }
+     })
+   })
+ }
+ else {
+  removeThis.remove()
+  alert("deleted!")
+ }
+ })}
+          
+          
+function loadPosts(snapshot) {
+  
+snapshot.forEach(function(childsnapshot){
+          var key = childsnapshot.key();
+          var childData = childsnapshot.val();
+        
+          var outerDiv = document.createElement('div');
+          outerDiv.id = "outer";
+          document.getElementsByClassName('content-display')[0].appendChild(outerDiv);
+          
+          var createPost = document.createElement('h2');
+          var createButtonA = document.createElement('button');
+          var createButtonB = document.createElement('button');
+          var createAuthor = document.createElement('p');
+
+          createPost.id = "post";
+          createButtonA.className = "votes";
+          createButtonA.textContent = "Approve";
+          createButtonA.dataset.id = key;
+          createButtonB.className = "votes";
+          createButtonB.textContent = "Deny";
+          createButtonB.dataset.id = key;
+          createAuthor.className = "detail-author";
+
+          createPost.innerHTML = childData.content;
+          createAuthor.innerHTML = childData.author;
+
+          outerDiv.appendChild(createAuthor);
+          outerDiv.appendChild(createPost);
+          outerDiv.appendChild(createButtonA);
+          outerDiv.appendChild(createButtonB);
+          votingFunction();
+        });}
+          
+          
+window.onload = function() {
+  
+  if (localStorage.authDataName == "@NguyenBrian3" || localStorage.authDataName == "@uydesai" || localStorage.authDataName == "@parad0cks" || localStorage.authDataName == "@jevinsidhu" ) {
+    $("#name").html("Welcome, " + localStorage.authDataName);
+  }
+  else {
+      $("#name").html("fuck outta here");
+  }
+  if ($("#sort-vote").val() == "sortByVotes") {
+      pendingRef.orderByPriority().once("value", function(snapshot)
+      {loadPosts(snapshot)
+      })
+    }
+  else {
+     pendingRef.orderByChild("date").once("value", function(snapshot)
+      {loadPosts(snapshot)
+      })
+  }
+};
+
+$("#login").click(function(){
+  
+  pendingRef.authWithOAuthPopup("twitter", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log(authData.twitter.username);
+      localStorage.authDataName = "@" + authData.twitter.username;
+      localStorage.authData = authData;
+      $("#name").html("Welcome, " + "@" + authData.twitter.username);
+    } 
+  }, {
+      remember: "default"
+  });
+
+
+});
+
+
+pendingRef.on("value", function(snapshot) {
+  var objectSnapshot = snapshot.val();
+  for(key in objectSnapshot) {
+    var loveToUpdate = $("p[data-id='" + key +"']");
+    var targetObject = snapshot.child(key).val()
+    loveToUpdate.html(targetObject.love);
+  }
+});
+
+  pendingRef.limitToLast(1).on("child_added", function(snapshot) {
+    if (!newItems) return;
+    newPost = snapshot.val();
+    var newPostKey = snapshot.key();
+    
+    console.log(newPostKey);
+    
+     var outerDiv = document.createElement('div');
+          outerDiv.id = "outer";
+          document.getElementsByClassName('content-display')[0].appendChild(outerDiv);
+          
+          var createPost = document.createElement('h2');
+          var createButtonA = document.createElement('button');
+          var createButtonB = document.createElement('button');
+          var createAuthor = document.createElement('p');
+
+          createPost.id = "post";
+          createButtonA.className = "votes";
+          createButtonA.textContent = "Approve";
+          createButtonA.dataset.id = key;
+          createButtonB.className = "votes";
+          createButtonB.textContent = "Deny";
+          createButtonB.dataset.id = key;
+          createAuthor.className = "detail-author";
+
+          createPost.innerHTML = newPost.content;
+          createAuthor.innerHTML = newPost.author;
+
+          outerDiv.appendChild(createAuthor);
+          outerDiv.appendChild(createPost);
+          outerDiv.appendChild(createButtonA);
+          outerDiv.appendChild(createButtonB);
+          votingFunction();
+});
+
+pendingRef.once('value', function(messages) {
+  newItems = true;
+});
+
+
--- js/script.js
+++ js/script.js
@@ -1,25 +1,59 @@
 /* global Firebase*/
-var ref = new Firebase("https://deepshit.firebaseio.com/");
+/* global votingFunction*/
+/* global sortSetting*/
+/* global userData*/
+var approvedRef = new Firebase("https://deepshit.firebaseio.com/approved");
+var pendingRef = new Firebase("https://deepshit.firebaseio.com/pending");
+
+
 
 var accessAuthData;
 var olderPost;
 var newPost;
 var newItems = false;
+var sortSetting
 
-window.onload = function() {
+if (sessionStorage.selection == "sortByDate") {
+  $("#dates").attr("selected", "selected")
+  sortSetting = "dates"
+}
+
+function votingFunction() {
+ $(".votes").unbind().on("click",function() {
+ if (localStorage.authDataName == undefined) {
+     alert("fuck outta here, sign in.")
+     return }
+     var idOfPost = this.dataset.id
+     var userPath = idOfPost + "/usersvoted" + "/" + localStorage.authDataName
+     var voteValue = idOfPost + "/love";
+      approvedRef.once('value', function(snapshot) {
+      if(snapshot.hasChild(userPath) == true) {
+        var removeThisPath = new Firebase('https://deepshit.firebaseio.com/' + userPath);
+         removeThisPath.remove()
+         approvedRef.child(voteValue).transaction(function(currentValue) {
+                  var newValue = currentValue-1;
+                  approvedRef.child(idOfPost).setPriority(-newValue);
+                  return newValue;
+              })}
+              else {
+                var voteList = approvedRef.child(idOfPost + "/usersvoted");
+                var objectToUpdate = {};
+                objectToUpdate[localStorage.authDataName] = "yes";
+                voteList.update(objectToUpdate);
+                approvedRef.child(voteValue).transaction(function(currentValue) {
+                  var newValue = currentValue+1;
+                  approvedRef.child(idOfPost).setPriority(-newValue);
+                  return newValue;
+                })}
+          })})}
+          
+          
+function loadPosts(snapshot) {
   
-  if (localStorage.authDataName == undefined) {
-    $("#name").html("hello stranger");
-  }
-  else {
-      $("#name").html("Welcome, " + localStorage.authDataName);
-  }
-      ref.once("value", function(snapshot) {
-        olderPost = snapshot.val();
+snapshot.forEach(function(childsnapshot){
+          var key = childsnapshot.key();
+          var childData = childsnapshot.val();
         
-        for(var key in olderPost){
-          console.log(key);
-            
           var outerDiv = document.createElement('div');
           outerDiv.id = "outer";
           document.getElementsByClassName('content-display')[0].appendChild(outerDiv);
@@ -37,81 +71,108 @@ window.onload = function() {
           createLove.className = "detail-love";
           createLove.dataset.id = key;
           
-          createPost.innerHTML = olderPost[key].content;
-          createAuthor.innerHTML = olderPost[key].author;
-          createLove.innerHTML = olderPost[key].love;
+          createPost.innerHTML = childData.content;
+          createAuthor.innerHTML = childData.author;
+          createLove.innerHTML = childData.love;
           
           outerDiv.appendChild(createAuthor);
           outerDiv.appendChild(createLove);
           outerDiv.appendChild(createPost);
           outerDiv.appendChild(createButton);
+          votingFunction();
+        });}
           
-          $(".votes").unbind().on("click",function() {
-            if (localStorage.authDataName == undefined) {
-              console.log("fuck outta here")
-              return}
-            var idOfPost = this.dataset.id
-            ref.once('value', function(snapshot) {
-              if(snapshot.hasChild(idOfPost + "/usersvoted" + "/" + localStorage.authDataName) == true) {
-                console.log("Already voted!")
-              } 
-              else {
-                var voteList = ref.child(idOfPost + "/usersvoted");
-                var objectToUpdate = {}
-                objectToUpdate[localStorage.authDataName] = "yes"
-                voteList.update(objectToUpdate);
-                var voteValue = idOfPost + "/love";
-                ref.child(voteValue).transaction(function(currentValue) {
-                  return currentValue+1;
-                })};
-          })});
-      }
-        
-      },function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-      });
+          
+window.onload = function() {
+  
+  // if (localStorage.authDataName == undefined) {
+  //   $("#name").html("hello stranger");
+  // }
+  // else {
+  //     $("#name").html("Welcome, " + localStorage.authDataName);
+  // }
+  
+  approvedRef.authWithOAuthToken("twitter", {
+  "user_id": localStorage.id,
+  "oauth_token": localStorage.accessToken,
+  "oauth_token_secret": localStorage.accessTokenSecret
+  }, function(error, authData) {
+   if (error) {
+     alert("Authenticated failed!", error)
+   } 
+   else {
+     $("#name").html("Welcome, " + "@" + authData.twitter.username);
+     $("#name").html("Welcome, " + "@" + authData.twitter.username);
+   }
+  })
+  if ($("#sort-vote").val() == "sortByVotes") {
+      approvedRef.orderByPriority().once("value", function(snapshot)
+      {loadPosts(snapshot)
+      })
+    }
+  else {
+     approvedRef.orderByChild("date").once("value", function(snapshot)
+      {loadPosts(snapshot)
+      })
+  }
 };
 
 $("#login").click(function(){
   
-  ref.authWithOAuthPopup("twitter", function(error, authData) {
+  // approvedRef.authWithOAuthPopup("twitter", function(error, authData) {
+  //   if (error) {
+  //     console.log("Login Failed!", error);
+  //   } else {
+  //     console.log(authData.twitter.username);
+  //     localStorage.authDataName = "@" + authData.twitter.username;
+  //     localStorage.authData = authData;
+  //     $("#name").html("Welcome, " + "@" + authData.twitter.username);
+  //   } 
+  // }, {
+  // });
+
+  approvedRef.authWithOAuthPopup("twitter", function(error, authData) {
     if (error) {
       console.log("Login Failed!", error);
     } else {
       console.log(authData.twitter.username);
-      localStorage.authDataName = "@" + authData.twitter.username;
-      localStorage.authData = authData;
+      localStorage.accessToken = authData.twitter.accessToken
+      localStorage.accessTokenSecret = authData.twitter.accessTokenSecret
+      localStorage.id = authData.twitter.id
       $("#name").html("Welcome, " + "@" + authData.twitter.username);
     } 
   }, {
-      remember: "default"
   });
-
 
 });
 
 $("#poster").click(function(){
-  
-  var newPostRef = ref.push({
-      author: localStorage.authDataName,
-      content: $("#content-input").val(),
-      love: 0
-  });
-  
+  if(localStorage.accessToken & localStorage.accessTokenSecret == undefined) { 
+   alert("fuck outta here, sign in.");
+  }
+  else {
+    var date = new Date;
+    var newPostRef = pendingRef.push({
+        author: approvedRef.getAuth().twitter.username,
+        content: $("#content-input").val(),
+        love: 0,
+        date: -date.getTime()
+    })
+    newPostRef.setPriority(0)
   document.getElementById("content-input").value = "";
   
-});
+}});
 
-ref.on("value", function(snapshot) {
+approvedRef.on("value", function(snapshot) {
   var objectSnapshot = snapshot.val();
   for(key in objectSnapshot) {
     var loveToUpdate = $("p[data-id='" + key +"']");
     var targetObject = snapshot.child(key).val()
-    loveToUpdate.html(targetObject.love)
+    loveToUpdate.html(targetObject.love);
   }
 });
 
-  ref.limitToLast(1).on("child_added", function(snapshot) {
+  approvedRef.orderByChild("date").limitToFirst(1).on("child_added", function(snapshot) {
     if (!newItems) return;
     newPost = snapshot.val();
     var newPostKey = snapshot.key();
@@ -119,9 +180,14 @@ ref.on("value", function(snapshot) {
     console.log(newPostKey);
     
     var outerDiv = document.createElement('div');
-    outerDiv.id = "outer";
-    document.getElementsByClassName('content-display')[0].appendChild(outerDiv);
-      
+    outerDiv.id = "outer"
+    if (sortSetting == "dates") {
+      document.getElementsByClassName('content-display')[0].insertBefore(outerDiv, document.getElementsByClassName('content-display')[0].childNodes[0]);
+      // this line is fucking stupid long but im lazy
+    }
+    else {
+      document.getElementsByClassName('content-display')[0].appendChild(outerDiv);
+    }
     var createPost = document.createElement('h2');
     var createButton = document.createElement('button');
     var createAuthor = document.createElement('p');
@@ -143,24 +209,15 @@ ref.on("value", function(snapshot) {
     outerDiv.appendChild(createPost);
     outerDiv.appendChild(createButton);
     
-    $(".votes").unbind().on("click",function() {
-            var voteValue = this.dataset.id + "/love";
-            
-            ref.child(voteValue).transaction(function(currentValue) {
-              return currentValue+1;
-            });
-          });
+    votingFunction();
 });
 
-ref.once('value', function(messages) {
+approvedRef.once('value', function(messages) {
   newItems = true;
 });
-// Messing up the clean code with your shit.
-// Fix something, break something else; sick.
 
-// var testRef = new Firebase("https://deepshit.firebaseio.com/-KB18yV92Dpn39DySFoV/usersvoted");
-// testRef.set({
-//   nguyenbrian: {
-//   voted: "yes"    
-//   }
-// })
+$("#sort-vote").change(function() {
+  sessionStorage.setItem("selection", $("#sort-vote").val())
+  location.reload()
+})
+
